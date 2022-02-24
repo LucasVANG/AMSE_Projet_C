0904d63dfefef7dfa15f08b92bfd709e764681b9
AMSE <oui@oui>
2022-02-24 09:50:00 +0100

    kine

--- src/lectempkine.c
+++ src/lectempkine.c
@@ -15,8 +15,7 @@
 /*....................*/
 /* variables globales */
 /*....................*/
-#define AREA_NAME       "TVR"    /* ->nom de la zone partagee                 */
-#define AREA_NAME2      "VELOCITY"
+#define AREA_NAME       "VELOCITY"    /* ->nom de la zone partagee                 */
 #define STOP            "A"      /* ->chaine a saisir pour declencher l'arret */
 #define STR_LEN         256         /* ->taille par defaut des chaines           */
 int test=0;
@@ -24,30 +23,23 @@ int  hh,                       /* ->heures                              */
      mm,                       /* ->minutes                             */
      ss;                       /* ->secondes                            */
 int  GoOn = 1;                 /* ->controle d'execution                */
-double W=0;
-double R=0;
 
 /*...................*/
 /* prototypes locaux */
 /*...................*/
-typedef struct etat_moteur{
-  double i;
-  double w;
-}etat_moteur;
+void usage( char *);           /* ->aide de ce programme                */
+void cycl_alm_handler( int );  /* ->gestionnaire pour l'alarme cyclique */
+/*&&&&&&&&&&&&&&&&&&&&&&*/
+/* aide de ce programme */
+/*&&&&&&&&&&&&&&&&&&&&&&*/
 
 typedef struct vitesse{
     double v;
     double w;
 }vitesse;
 
-char *szInStr;
-vitesse *szInStr2; 
+vitesse *szInStr;
 
-void usage( char *);           /* ->aide de ce programme                */
-void cycl_alm_handler( int );  /* ->gestionnaire pour l'alarme cyclique */
-/*&&&&&&&&&&&&&&&&&&&&&&*/
-/* aide de ce programme */
-/*&&&&&&&&&&&&&&&&&&&&&&*/
 void usage( char *pgm_name )
 {
   if( pgm_name == NULL )
@@ -65,25 +57,11 @@ void usage( char *pgm_name )
 /*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
 void cycl_alm_handler( int signal ) //On lit la mémoire partagé a chaque itération
 {
-    etat_moteur testMot;
-    testMot.i=1;
-    testMot.w=1;
-    etat_moteur testMot2;
-    testMot2.i=1;
-    testMot2.w=1;
-    vitesse res;
-    res.v=(testMot.w+testMot2.w)*0.5*R;
-    res.w=(testMot.w-testMot2.w)*(1/W);
-    *szInStr2=res;
     test++;
     printf("%i\n", test);
-
-    
     /* affichage */
-    printf("contenu de la zone = %s\n", szInStr);
-    printf("W=%f,R=%f\n",W,R);
-    printf("v=%f,w=%f\n",res.v,res.w);
-    
+    printf("contenu de la zone = %f %f\n", szInStr->v,szInStr->w);
+    //shm_unlink(AREA_NAME);
 }
 
 
@@ -96,14 +74,7 @@ int main( int argc, char *argv[])
                         sa_old;  /* ->ancienne config de gestion d'alarme     */
   sigset_t              blocked; /* ->liste des signaux bloques               */
   struct itimerval      period;  /* ->periode de l'alarme cyclique            */
-  /*verif arguments*/
-  if(argc!=4){
-      printf("Nombre arguments invalides");
-      return (0);
-  }
   /* initialisation */
-  W=atof(argv[1]);
-  R=atof(argv[2]);
   sigemptyset( &blocked );
   memset( &sa, 0, sizeof( sigaction )); /* ->precaution utile... */
   sa.sa_handler = cycl_alm_handler;
@@ -112,29 +83,16 @@ int main( int argc, char *argv[])
   /* installation du gestionnaire de signal */
   sigaction(SIGALRM, &sa, NULL );
   /* initialisation de l'alarme  */
-  
- 
-  if((int)atof(argv[3])>=100){
-    printf("%i",(int)atof(argv[3])/100);
-    period.it_interval.tv_sec  = (int)atof(argv[3])/100;  
-    period.it_interval.tv_usec = 0;
-  }
-  else {
-    printf("%f",atof(argv[3])*10000);
-    period.it_interval.tv_sec  = 0;  
-    period.it_interval.tv_usec = (double)atof(argv[3])*10000;
-  }
- 
+  period.it_interval.tv_sec  = 1;  
+  period.it_interval.tv_usec = 0;
   period.it_value.tv_sec     = 1;
   period.it_value.tv_usec    = 0;
   /* demarrage de l'alarme */
   setitimer( ITIMER_REAL, &period, NULL );
   /* on ne fait desormais plus rien d'autre que */
   /* d'attendre les signaux                     */
-    void *vAddr;                    /* ->adresse virtuelle sur la zone          */
-    void *vAddr2;                
+  void *vAddr;                    /* ->adresse virtuelle sur la zone          */                  /* ->chaine saisie                          */
     int  iShmFd;                    /* ->descripteur associe a la zone partagee */
-    int  iShmFd2;
     /*..................................*/
     /* tentative d'acces a la zone */
     /*..................................*/
@@ -155,38 +113,13 @@ int main( int argc, char *argv[])
         fprintf(stderr,"         code  = %d (%s)\n", errno, (char *)(strerror(errno)));
         return( -errno );
     };
-
-    // ecriture vitesse
-    if( (iShmFd2 = shm_open(AREA_NAME2, O_RDWR | O_CREAT, 0600)) < 0)
-    {
-        /* on essaie de se lier sans creer... */
-        printf("echec de creation, lien seul...\n");
-        if( (iShmFd2 = shm_open(AREA_NAME2, O_RDWR, STR_LEN)) < 0)
-        {  
-            fprintf(stderr,"ERREUR : ---> appel a shm_open()\n");
-            fprintf(stderr,"         code  = %d (%s)\n", errno, (char *)(strerror(errno)));
-            return( -errno );
-        };
-    };
-    /* on attribue la taille a la zone partagee */
-    ftruncate(iShmFd2, STR_LEN);
-    /* tentative de mapping de la zone dans l'espace memoire du */
-    /* processus                                                */
-    if( (vAddr2 = mmap(NULL, STR_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, iShmFd2, 0 ))  == NULL)
-    {
-        fprintf(stderr,"ERREUR : ---> appel a mmap()\n");
-        fprintf(stderr,"         code  = %d (%s)\n", errno, (char *)(strerror(errno)));
-        return( -errno );
-    };
-    szInStr2 = (vitesse *)(vAddr2);
-    szInStr = (char *)(vAddr);
+    szInStr = (vitesse *)(vAddr);
   do
   {
-    
     pause();
   }
   while( GoOn == 1 );
   /* fini */
   printf("FIN DU DECOMPTE.\n");
-  return( 0 );
+  return( 0 );  /* ->on n'arrive pas jusque la en pratique */
 }
